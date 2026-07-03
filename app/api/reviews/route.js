bce9eb0f2149769dabfc2af6f51568a0cac3d527
orishainfinity <159387754+orishainfinity@users.noreply.github.com>
2026-07-02 21:06:56 -0500

    disable caching on reviews API

--- app/api/reviews/route.js
+++ app/api/reviews/route.js
@@ -1,6 +1,8 @@
 import { sql } from '@/lib/db'
 
 export const dynamic = 'force-dynamic'
+export const revalidate = 0
+export const fetchCache = 'force-no-store'
 
 export async function GET() {
   try {
